91d3ffd4de5c1f9db78eab2c58b4d3569aec248e
Sneha T <thisisrahul8015@gmail.com>
2025-07-19 18:14:01 +0530

    debug

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,20 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    if Rails.env.production?
+      puts "--- CHECKING ENV VARS ON RENDER ---"
+      puts "LINKEDIN_CLIENT_ID is present? #{ENV['LINKEDIN_CLIENT_ID'].present?}"
+      puts "LINKEDIN_CLIENT_SECRET is present? #{ENV['LINKEDIN_CLIENT_SECRET'].present?}"
+      puts "-------------------------------------"
+    end
+
+    # Add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :good_job
+
+    # Configure OmniAuth middleware
+   
   end
 end
